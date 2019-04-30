1f06eb9c500e6265725204fc17b91108d8b9329c
james-braun <braun.james.robert@gmail.com>
2019-04-30 11:30:27 -0500

    add function and comments

--- js/script.js
+++ js/script.js
@@ -5,40 +5,17 @@ FSJS project 2 - List Filter and Pagination
    
 // Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
 
-
-/*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
-   
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
-***/
+// Holds list of students from html.
 const studentList = document.getElementsByClassName("student-list")[0].getElementsByTagName("li");
-var page = 1;
-var listSize = 10;
-var numberOfPageLinks = Math.ceil(studentList.length / listSize);
 
+const showPage = (list, page, pageSize) => {
 
-/*** 
-   Create the `showPage` function to hide all of the items in the 
-   list except for the ten you want to show.
-
-   Pro Tips: 
-     - Keep in mind that with a list of 54 students, the last page 
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when 
-       you initially define the function, and it acts as a variable 
-       or a placeholder to represent the actual function `argument` 
-       that will be passed into the parens later when you call or 
-       "invoke" the function 
-***/
-const showPage = (list, pageSize) => {
+    // Set beginning and end indexes.
     var startIndex = page * pageSize - pageSize;
     var endIndex = page * pageSize;
+
+    // itterate though list of stuents and show and hide them
+    // based on whether they are between the indexes.
     for (var i = 0; i < list.length; i += 1) {
         if (i < startIndex || i >= endIndex) {
             list[i].style.display = "none";
@@ -48,19 +25,17 @@ const showPage = (list, pageSize) => {
     }
 }
 
-showPage(studentList, listSize);
+const createPagination = (page, numberOfPageLinks) => {
 
-/*** 
-   Create the `appendPageLinks function` to generate, append, and add 
-   functionality to the pagination buttons.
-***/
+    var element = document.createElement('div')  // Holds element to be created.
 
-
-const createelement = () => {
-
-    var element = document.createElement('div')
+    // Add a class name to the element.
     element.className = "pagination";
+
+    // Create ul element.
     element.innerHTML = `<ul>`;
+
+    // create each li and 'a' element inside ul element.
     for (var i = 1; i <= numberOfPageLinks; i += 1) {
         if (i === page) {
             element.innerHTML += `<li><a class="active" href="#">${i}</a></li>`;
@@ -68,24 +43,49 @@ const createelement = () => {
             element.innerHTML += `<li><a href="#">${i}</a></li>`;
         }
     }
+
+    // Close ul element.
     element.innerHTML += `</ul>`;
+
+    // Add element to the page.
     document.getElementsByClassName("page")[0].appendChild(element);
 }
 
-createelement();
-var element = document.getElementsByClassName('pagination')[0].getElementsByTagName('li');
-for (var i = 0; i < numberOfPageLinks; i += 1) {
-    element[i].addEventListener('click', function () {
-        page = parseInt(this.textContent);
-        console.log(page);
-        console.log(element);
-        for (var j = 0; j < element.length; j += 1) {
-            element[j].getElementsByTagName('a')[0].className = "";
-        }
-        this.getElementsByTagName('a')[0].className = "active";
-        showPage(studentList, listSize);
+const appendPageLinks = (studentList) => {
 
-    });
-};
+    var page = 1;  // Which page is being displayed.
+    var listSize = 10; // the maximum students per page.
+    var numberOfPageLinks = Math.ceil(studentList.length / listSize); // The number of pagination links.
 
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
+    // Show one page of students.
+    showPage(studentList, page, listSize);
+
+    // Create pagination links for that page.
+    createPagination(page, numberOfPageLinks);
+
+    // Holds list of li elements in pagination div.
+    var element = document.getElementsByClassName('pagination')[0].getElementsByTagName('li');
+
+    // create event listener for each li in pagination.
+    for (var i = 0; i < numberOfPageLinks; i += 1) {
+        element[i].addEventListener('click', function () {
+
+            // Set the page varible to the paginations link's number value.
+            page = parseInt(this.textContent);
+
+            // clear the "active" or selected class from li's in pagination.
+            for (var j = 0; j < element.length; j += 1) {
+                element[j].getElementsByTagName('a')[0].className = "";
+            }
+
+            // Add active class to current page's pagination link.
+            this.getElementsByTagName('a')[0].className = "active";
+
+            // Show current page.
+            showPage(studentList, page, listSize);
+
+        });
+    };
+}
+
+appendPageLinks(studentList);
